11efd1a13f7a89eb174f3f87a84e2bf441d32f30
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-09-10 15:45:17 +0300

    implement base functionality for scraping

--- scraping/dou/spiders/vacancies.py
+++ scraping/dou/spiders/vacancies.py
@@ -1,4 +1,7 @@
 import scrapy
+from scrapy.http import Response
+
+from scraping.dou.config import python_technologies
 
 
 class VacanciesSpider(scrapy.Spider):
@@ -6,5 +9,18 @@ class VacanciesSpider(scrapy.Spider):
     allowed_domains = ["jobs.dou.ua"]
     start_urls = ["https://jobs.dou.ua/vacancies/?category=Python"]
 
-    def parse(self, response):
-        pass
+    def get_detail_page(self, response: Response, **kwargs):
+        for vacancy in response.css(".l-vacancy"):
+            detail_page_url = vacancy.css(".vt::attr(href)").get()
+            self.log(f"Found detail page: {detail_page_url}")
+            if detail_page_url:
+                yield scrapy.Request(detail_page_url, callback=self.parse_detail)
+
+    def parse(self, response: Response, **kwargs):
+        yield from self.get_detail_page(response, **kwargs)
+
+    def parse_detail(self, response: Response):
+        yield {
+            "title": response.css("h1::text").get(),
+            "company": response.css(".l-n > a::text").get(),
+        }
