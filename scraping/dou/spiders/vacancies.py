9d1732f0757f5ea2995d5c6762f52e7071d9091d
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-09-10 22:54:34 +0300

    add selenium

--- scraping/dou/spiders/vacancies.py
+++ scraping/dou/spiders/vacancies.py
@@ -1,13 +1,28 @@
-import scrapy
-from scrapy.http import Response
+import time
+from typing import Any, Union
 
-from scraping.dou.config import python_technologies
+import scrapy
+from scrapy import Spider
+from scrapy.http import Response, HtmlResponse
+from selenium import webdriver
+from selenium.common import NoSuchElementException, ElementClickInterceptedException, ElementNotInteractableException
+from selenium.webdriver.common.by import By
+from twisted.internet.defer import Deferred
+
+from ..config import python_technologies
 
 
 class VacanciesSpider(scrapy.Spider):
     name = "vacancies"
     allowed_domains = ["jobs.dou.ua"]
     start_urls = ["https://jobs.dou.ua/vacancies/?category=Python"]
+
+    def __init__(self, **kwargs: Any):
+        super().__init__(**kwargs)
+        self.driver = webdriver.Chrome()
+
+    def close(self, reason: str):
+        self.driver.close()
 
     def get_detail_page(self, response: Response, **kwargs):
         for vacancy in response.css(".l-vacancy"):
@@ -17,10 +32,37 @@ class VacanciesSpider(scrapy.Spider):
                 yield scrapy.Request(detail_page_url, callback=self.parse_detail)
 
     def parse(self, response: Response, **kwargs):
-        yield from self.get_detail_page(response, **kwargs)
+        self.driver.get(response.url)
+
+        while True:
+            try:
+                more_button = self.driver.find_element(
+                    By.CSS_SELECTOR, ".more-btn a"
+                )
+                more_button.click()
+                time.sleep(2)
+            except (NoSuchElementException, ElementClickInterceptedException, ElementNotInteractableException):
+                break
+
+        page_source = self.driver.page_source
+
+        selenium_response = HtmlResponse(
+            url=response.url,
+            body=page_source,
+            encoding="utf-8"
+        )
+
+        yield from self.get_detail_page(selenium_response, **kwargs)
 
     def parse_detail(self, response: Response):
+
+        full_text = response.css(".b-typo *::text").getall()
+        full_text = " ".join(full_text).lower()
+
+        found_technologies = [tech for tech in python_technologies if tech.lower() in full_text]
+
         yield {
             "title": response.css("h1::text").get(),
             "company": response.css(".l-n > a::text").get(),
+            "technologies": found_technologies
         }
